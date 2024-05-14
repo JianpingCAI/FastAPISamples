1efc34a4eaed34d25f495b6eed4d82ee2ea8fd98
JianpingCai <cai-jianping@outlook.com>
2024-05-14 15:41:33 +0800

    removed the deprecated Config in pydantic

--- unittest_fastapi/fastapi_crud/app/schemas.py
+++ unittest_fastapi/fastapi_crud/app/schemas.py
@@ -1,5 +1,5 @@
 # app/schemas.py
-from pydantic import BaseModel
+from pydantic import BaseModel, ConfigDict
 from typing import Optional
 
 
@@ -15,5 +15,6 @@ class ItemCreate(ItemBase):
 class Item(ItemBase):
     id: int
 
-    class Config:
-        from_attributes = True
+    # class Config:
+    #     from_attributes = True
+    model_config = ConfigDict(from_attributes=True)
