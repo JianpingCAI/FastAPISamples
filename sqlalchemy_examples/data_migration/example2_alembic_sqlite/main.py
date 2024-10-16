0a8d2299e37fe16eab0fc007f30a7f1751f94969
JianpingCai <cai-jianping@outlook.com>
2024-10-17 00:37:36 +0800

    update

---
+++ sqlalchemy_examples/data_migration/example2_alembic_sqlite/main.py
@@ -0,0 +1,27 @@
+from orm.models import sessionmaker, engine, Customer, Order
+
+# Create a sessionmaker bound to the engine
+Session = sessionmaker(bind=engine)
+
+# Create a session
+session = Session()
+
+# Example usage
+# new_customer = Customer(name="John Doe", email="p5kzU@example.com")
+# session.add(new_customer)
+# session.commit()
+
+print("-------------Customers-------------")
+
+# Querying data
+customers = session.query(Customer).all()
+for customer in customers:
+    # customer.orders.append(Order(item_name="Item 1", customer_id=customer.id))
+    print(customer.id, customer.name, customer.email, customer.address)
+
+# session.commit()
+
+print("-------------Orders-------------")
+orders = session.query(Order).all()
+for order in orders:
+    print(order.id, order.item_name, order.customer_id)
--- sqlalchemy_examples/data_migration/example2_alembic_sqlite/orm/models.py
+++ sqlalchemy_examples/data_migration/example2_alembic_sqlite/orm/models.py
@@ -13,34 +13,20 @@ from sqlalchemy.orm import relationship, sessionmaker
 # Declare the base
 Base = declarative_base()
 
-custom_id_seq = Sequence("custom_id_seq")
-order_id_seq = Sequence("order_id_seq")
-
 
 class Customer(Base):
     __tablename__ = "customers"
 
-    # id = Column(Integer, primary_key=True)
-    id = Column(
-        Integer,
-        custom_id_seq,
-        server_default=custom_id_seq.next_value(),
-        primary_key=True,
-    )
+    id = Column(Integer, primary_key=True)
     name = Column(String, nullable=False)
     email = Column(String, nullable=False)
+    address = Column(Text, nullable=True)
 
 
 class Order(Base):
     __tablename__ = "orders"
 
-    # id = Column(Integer, primary_key=True)
-    id = Column(
-        Integer,
-        order_id_seq,
-        server_default=order_id_seq.next_value(),
-        primary_key=True,
-    )
+    id = Column(Integer, primary_key=True)
     item_name = Column(String, nullable=False)
     customer_id = Column(Integer, ForeignKey("customers.id"), nullable=False)
     customer = relationship("Customer", back_populates="orders")
@@ -49,9 +35,14 @@ class Order(Base):
 Customer.orders = relationship("Order", order_by=Order.id, back_populates="customer")
 
 # Set up the database engine
-engine = create_engine("duckdb:///test_duckdb.db", echo=False)
+engine = create_engine("sqlite:///./test_sqlite.db", echo=True)
+
 
 # Create all tables in the database
-Base.metadata.create_all(engine)
+def create_database():
+    Base.metadata.create_all(engine)
 
-print("********************Tables created successfully.")
+
+# Base.metadata.create_all(engine)
+
+# print("********************Tables created successfully.")
